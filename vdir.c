2341b4a96c130b485202dc5ee640d57701e61e13
Vlad-Valean <vlad.valean19@gmail.com>
2024-04-09 13:15:26 +0300

    code refactoring

--- vdir.c
+++ vdir.c
@@ -22,31 +22,33 @@ void snap_file(char *path) {
     printf("%d\n", open(snap_path, O_EXCL | O_WRONLY | O_TRUNC, S_IWOTH));
 }
 
+int dot_folder_validation(const char *path) {
+    return !strcmp(path, ".\0") || !strcmp(path, "..\0");
+}
+
+void update_path(char* new_path, const char *old_path, const char *path_extender) {
+    strcpy(new_path, old_path);
+    strcat(new_path, "/\0");
+    strcat(new_path, path_extender);
+}
 
 void rec_parse(DIR *directory_obj, const char* path) {
     struct dirent *directory_content = NULL;
     DIR *child_directory_obj = NULL;
-    printf("\nFOLDER:  %s:\n\n", path);
     while((directory_content = readdir(directory_obj))) {
 
-        char child_directory_path[120] = "\0";
-        strcpy(child_directory_path, path);
-        strcat(child_directory_path, "/\0");
-        strcat(child_directory_path, directory_content->d_name);
+        char child_directory_path[240];
+        update_path(child_directory_path, path, directory_content->d_name);
         
-        if(!strcmp(directory_content->d_name, ".\0") || !strcmp(directory_content->d_name, "..\0")) {
+        if(dot_folder_validation(directory_content->d_name)) {
         } else {
-            char new_path[120];
-            strcpy(new_path, "/\0");
-            strcat(new_path, directory_content->d_name);
+            
+            printf("%s\n", child_directory_path);
             if((child_directory_obj = opendir(child_directory_path))) {
-
-            rec_parse(child_directory_obj, new_path);
-            printf("\n");
-            //altfel ....
+            rec_parse(child_directory_obj, child_directory_path);
+            
             } else {
-                printf("file:  %s\n", new_path);
-                snap_file(new_path);
+                snap_file(child_directory_path);
             }
         } 
 
